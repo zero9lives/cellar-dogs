ef182b5a85b91e182dfd9405022acc32e9b019a6
094 <geopark25@gmail.com>
2025-08-30 19:46:05 -0400

    Add files via upload

---
+++ cellardogs-website2/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Just one puzzle state
+let puzzleSolved = false;
+
+// Mark puzzle as solved
+app.post('/solve', (req, res) => {
+    puzzleSolved = true;
+    console.log("Puzzle solved!");
+    res.json({ success: true });
+});
+
+// Check puzzle state
+app.get('/check', (req, res) => {
+    res.json({ solved: puzzleSolved });
+});
+
+app.listen(3000, () => console.log("Server running on https://zero9lives.github.io/cellar-dogs/cellardogs-website"));
